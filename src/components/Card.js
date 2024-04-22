615d68cb6128c724c94a0aff9816101b64d51c7b
helal <helal.h.hridoy@gmail.com>
2024-04-22 21:06:42 +0600

    Header and footer added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Card() {
+export const Card = () => {
     return (
         <div>Card</div>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,30 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 
-function Footer() {
+export const Footer = () => {
     return (
-        <footer>Footer</footer>
+
+
+        <footer className="bg-white shadow  dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="https://flowbite.com/" className="hover:underline">CineMate</Link>. All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <Link to="#" className="hover:underline me-4 md:me-6">Facebook</Link>
+                    </li>
+                    <li>
+                        <Link to="#" className="hover:underline me-4 md:me-6">LinkedIn</Link>
+                    </li>
+                    <li>
+                        <Link to="#" className="hover:underline me-4 md:me-6">GitHub</Link>
+                    </li>
+                    <li>
+                        <Link to="#" className="hover:underline">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+
     )
 }
-
-export default Footer
